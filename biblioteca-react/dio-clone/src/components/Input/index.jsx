1328237e8742a65b716b01b20a58f739d12e712a
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-10-22 16:49:50 -0300

    feat: login and validation with json server

--- biblioteca-react/dio-clone/src/components/Input/index.jsx
+++ biblioteca-react/dio-clone/src/components/Input/index.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { IconContainer, InputText, InputContainer } from './styles';
+import { Controller } from "react-hook-form";
 
-const Input = ({leftIcon, name, ...rest}) => {
+import { IconContainer, InputText, InputContainer, ErrorText } from "./styles";
+
+const Input = ({ leftIcon, name, control, errorMessage, ...rest }) => {
   return (
-    <InputContainer>
-        {leftIcon ? <IconContainer>{ leftIcon }</IconContainer> : null}
-        <InputText {...rest}/>
-    </InputContainer>
+    <>
+      <InputContainer>
+        {leftIcon ? <IconContainer>{leftIcon}</IconContainer> : null}
+        <Controller
+          name={name}
+          control={control}
+          render={({ field }) => <InputText {...field} {...rest} />}
+        />
+      </InputContainer>
 
+      {errorMessage ? <ErrorText>{errorMessage}</ErrorText> : null}
+    </>
   );
-}
+};
 
 export { Input };
--- biblioteca-react/dio-clone/src/pages/login/index.jsx
+++ biblioteca-react/dio-clone/src/pages/login/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { MdEmail, MdLock } from "react-icons/md";
+
+import { api } from "../../services/api";
+
 import { useNavigate } from "react-router-dom";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
-
-import { MdEmail, MdLock } from 'react-icons/md';
 
 import {
   Column,
@@ -20,13 +26,45 @@ import {
   Wrapper,
 } from "./styles";
 
+const schema = yup
+  .object({
+    email: yup.string().email("e-mail inválido").required("email errado"),
+    password: yup
+      .string()
+      .min(3, "No mínimo 3 caracteres")
+      .required("senha ruim"),
+  })
+  .required();
+
 export default function Login() {
-  
   const navigate = useNavigate();
 
-  const handleClickSignin = () => {
-    navigate("/feed")
-  }
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  const onSubmit = async (formData) => {
+    try {
+      const { data } = await api.get(
+        `users?email${formData.email}&senha${formData.password}`
+      );
+
+      // console.log(data.length)
+
+      if (data.length !== 0) {
+        navigate("/feed");
+      } else {
+        alert("Email ou senha inválido");
+      }
+    } catch (error) {
+      alert("Houve um erro, tente novamente");
+    }
+  };
 
   return (
     <>
@@ -42,11 +80,30 @@ export default function Login() {
           <Wrapper>
             <TitleLogin>Faça o seu cadastro</TitleLogin>
             <SubTitleLogin>Faça o seu login e make the change._</SubTitleLogin>
-            <form>
-              <Input placeholder="E-mail" type="email" leftIcon={<MdEmail/>}/>
-              <Input placeholder="Senha" type="password" leftIcon={<MdLock/>}/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                name="email"
+                errorMessage={errors?.email?.message}
+                control={control}
+                placeholder="E-mail"
+                type="email"
+                leftIcon={<MdEmail />}
+              />
+              <Input
+                name="password"
+                errorMessage={errors?.password?.message}
+                control={control}
+                placeholder="Senha"
+                type="password"
+                leftIcon={<MdLock />}
+              />
               {/* type="button" vai servir para evitar de fazer o subbmit  */}
-              <Button title="Entrar" variant="secondary" onClick={handleClickSignin} type="button" /> 
+              <Button
+                title="Entrar"
+                variant="secondary"
+                // onClick={handleClickSignin}
+                type="submit"
+              />
             </form>
             <Row>
               <EsqueciText>Esqueci minha senha</EsqueciText>
@@ -58,5 +115,3 @@ export default function Login() {
     </>
   );
 }
-
-// export { Login };
